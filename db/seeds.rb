d7a2d562f209591e314015a7a848f2e4aed2ef36
Ali Jendoubi <alijandhd@gmail.com>
2022-09-12 11:09:25 +0100

    📋Create Models , migrations and routes

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
+#   Character.create(name: "Luke", movie: movies.first)
+
+Greeting.create ([
+    { greeting: "The friend is the man who knows all about you, and still likes you." },
+    { greeting: "Lack of emotion causes lack of progress and lack of motivation." },
+    { greeting: "Live as if you were to die tomorrow" },
+    { greeting: "We must not allow other people's limited perceptions to define us" },  
+    { greeting: "If you cannot do great things, do small things in a great way" },
+    { greeting: "This above all: to thine own self be true" },
+    { greeting: "Be yourself; everyone else is already taken." }
+])
